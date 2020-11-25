bddf390988711569df1b6cb9855ad1ed96fed54a
Khizar-PC <khizerdawood6@gmail.com>
2020-11-26 00:47:28 +0500

    create separate for constants || Lecture 64

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -2,77 +2,93 @@ import 'package:flutter/material.dart';
 import 'Repeate_contanier.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'RepeateTextIcon.dart';
+import 'constent_variable.dart';
+
 enum Gender {
   male,
   female,
 }
-const activationcolor = Color(0xFF111328);
-const deactivationcolor = Color(0xFF1D1E33);
+
+
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
-
 class _InputPageState extends State<InputPage> {
-  Color maleColor = deactivationcolor;
   /*Color maleColor = deactivationcolor;
   Color femaleColor = deactivationcolor;
   void updateColor(Gender genderType) {
     if (genderType == Gender.male) {
-@@ -28,7 +28,8 @@ class _InputPageState extends State<InputPage> {
+      maleColor = activationcolor;
+      femaleColor = deactivationcolor;
+    }
+    if (genderType == Gender.female) {
       maleColor = deactivationcolor;
       femaleColor = activationcolor;
     }
-  }
   }*/
   Gender selectGender;
-
   @override
   Widget build(BuildContext context) {
-    @@ -45,11 +46,14 @@ class _InputPageState extends State<InputPage> {
-    child: GestureDetector(
-    onTap: () {
-    setState(() {
-    updateColor(Gender.male);
-    //updateColor(Gender.male);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI'),
+      ),
+      body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+      Expanded(
+      child: Row(
+      children: [
+          Expanded(
+          /*child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        //updateColor(Gender.male);
+                        selectGender = Gender.male;
+                      });
+                    },*/
+          child: Repeate_contanier(
+          onpressed: () {
+    setState(
+    () {
     selectGender = Gender.male;
-    });
     },
-    child: Repeate_contanier(
-    colors: maleColor,
-    colors: selectGender == Gender.male
-    ? activationcolor
-        : deactivationcolor,
-    cardWidge: RepeateTextIcon(
-    iconData: FontAwesomeIcons.male,
-    Label: "MALE",
-    @@ -61,11 +65,14 @@ class _InputPageState extends State<InputPage> {
-    child: GestureDetector(
-    onTap: () {
-    setState(() {
-    updateColor(Gender.female);
-    //updateColor(Gender.female);
-    selectGender = Gender.female;
-    });
+    );
     },
+      colors: selectGender == Gender.male
+          ? activationcolor
+          : deactivationcolor,
+      cardWidge: RepeateTextIcon(
+        iconData: FontAwesomeIcons.male,
+        Label: "MALE",
+      ),
+    ),
+    // ),
+    ),
+    Expanded(
+    /*child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        //updateColor(Gender.female);
+                        selectGender = Gender.female;
+                      });
+                    },*/
     child: Repeate_contanier(
-    colors: femaleColor,
-    colors: selectGender == Gender.female
-    ? activationcolor
-        : deactivationcolor,
-    cardWidge: RepeateTextIcon(
-    iconData: FontAwesomeIcons.female,
-    Label: "FEMALE",
-    ),
-    ),
-    ),
-    ),
-    ],
-    ),
-    ),
+    onpressed: () {
+    setState(
+    () {
+    @@ -101,6 +100,12 @@
     Expanded(
     child: Repeate_contanier(
     colors: Color(0xFF1D1E33),
+    cardWidge: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text('HEIGHT', style: ConstentTextLabel),
+    ],
+    ),
     ),
     ),
     Expanded(
@@ -96,3 +112,16 @@ class _InputPageState extends State<InputPage> {
     );
     }
     }
+    6  lib/RepeateTextIcon.dart
+    @@ -1,4 +1,5 @@
+    import 'package:flutter/material.dart';
+    import 'constent_variable.dart';
+
+    class RepeateTextIcon extends StatelessWidget {
+    RepeateTextIcon({@required this.iconData, this.Label});
+    @@ -17,10 +18,7 @@ class RepeateTextIcon extends StatelessWidget {
+    SizedBox(height: 10),
+    style: ConstentTextLabel,
+    )
+    ],
+    );
