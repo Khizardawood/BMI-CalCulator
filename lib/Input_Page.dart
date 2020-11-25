d7d30969e643d78353fa6adc327cb73da8c547c3
Khizar-PC <khizerdawood6@gmail.com>
2020-11-26 00:31:14 +0500

    Repeate contaner save as new library code in flutter || Lecture 57

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -3,8 +3,57 @@ import 'Repeate_contanier.dart';
 
 class InputPage extends StatefulWidget {
   @override
-  @@ -56,16 +57,3 @@ class _InputPageState extends State<InputPage> {
-  );
+  _InputPageState createState() => _InputPageState();
+}
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Repeate_contanier(
+              colors: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: Repeate_contanier(
+                    colors: Color(0xFF1D1E33),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
