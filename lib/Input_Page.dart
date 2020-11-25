b39ea0c29226996390971b165822282f4f65e52b
Khizar-PC <khizerdawood6@gmail.com>
2020-11-26 00:26:55 +0500

    How to refactor code in flutter || Lecture 57

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -1,91 +1,22 @@
 import 'package:flutter/material.dart';
+import 'Repeate_contanier.dart';
+
 class InputPage extends StatefulWidget {
   @override
-  _InputPageState createState() => _InputPageState();
-}
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI'),
-      ),
-      body: Column(
-          children: [
-      Expanded(
-      child: Row(
-      children: [
-          Expanded(
-          child: Container(
-          margin: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-          color: Color(0xFF1D1E33),
-          borderRadius: BorderRadiusDirectional.circular(15.0)),
-      child: Repeate_contanier(
-        colors: Color(0xFF1D1E33),
-      ),
-    ),
-    Expanded(
-    child: Container(
-    margin: EdgeInsets.all(10.0),
-    decoration: BoxDecoration(
-    color: Color(0xFF1D1E33),
-    borderRadius: BorderRadiusDirectional.circular(15.0)),
-    child: Repeate_contanier(
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    ],
-    ),
-    ),
-    Expanded(
-    child: Container(
-    margin: EdgeInsets.all(10.0),
-    decoration: BoxDecoration(
-    color: Color(0xFF1D1E33),
-    borderRadius: BorderRadiusDirectional.circular(15.0)),
-    child: Repeate_contanier(
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    Expanded(
-    child: Row(
-    children: [
-    Expanded(
-    child: Container(
-    margin: EdgeInsets.all(10.0),
-    decoration: BoxDecoration(
-    color: Color(0xFF1D1E33),
-    borderRadius: BorderRadiusDirectional.circular(15.0)),
-    child: Repeate_contanier(
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    Expanded(
-    child: Container(
-    margin: EdgeInsets.all(10.0),
-    decoration: BoxDecoration(
-    color: Color(0xFF1D1E33),
-    borderRadius: BorderRadiusDirectional.circular(15.0)),
-    child: Repeate_contanier(
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    ],
-    @@ -71,3 +56,16 @@ class _InputPageState extends State<InputPage> {
-    );
-    }
+  @@ -56,16 +57,3 @@ class _InputPageState extends State<InputPage> {
+  );
   }
+}
 
-  class Repeate_contanier extends StatelessWidget {
+class Repeate_contanier extends StatelessWidget {
   Repeate_contanier({@required this.colors});
   final Color colors;
   @override
   Widget build(BuildContext context) {
-  return Container(
-  margin: EdgeInsets.all(10.0),
-  decoration: BoxDecoration(
-  color: colors, borderRadius: BorderRadiusDirectional.circular(15.0)),
-  );
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: colors, borderRadius: BorderRadiusDirectional.circular(15.0)),
+    );
   }
-  }
+}
