746e2c3d5cfb52043d2e3baf281c3c970bd83232
Khizar-PC <khizerdawood6@gmail.com>
2020-11-26 01:09:13 +0500

    FLOATING ACTION BUTTON || lecture 67

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -3,14 +3,10 @@ import 'Repeate_contanier.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'RepeateTextIcon.dart';
 import 'constent_variable.dart';
-
 enum Gender {
   male,
   female,
 }
-
-
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -29,6 +25,9 @@ class _InputPageState extends State<InputPage> {
     }
   }*/
   Gender selectGender;
+  int slider_height = 180;
+  int slider_weight = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,22 +67,34 @@ class _InputPageState extends State<InputPage> {
     // ),
     ),
     Expanded(
-    cardWidge: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    Text(
-    'WEGHT',
-    style: ConstentTextLabel,
-    ),
-    Text(slider_weight.toString(),
-    style: ConstentTextLabe2),
-    ],
-    ),
+    /*child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        //updateColor(Gender.female);
+                        selectGender = Gender.female;
+                      });
+                    },*/
     child: Repeate_contanier(
     onpressed: () {
     setState(
     () {
-    @@ -101,6 +100,12 @@
+    selectGender = Gender.female;
+    },
+    );
+    },
+    colors: selectGender == Gender.female
+    ? activationcolor
+        : deactivationcolor,
+    cardWidge: RepeateTextIcon(
+    iconData: FontAwesomeIcons.female,
+    Label: "FEMALE",
+    ),
+    ),
+    //),
+    ),
+    ],
+    ),
+    ),
     Expanded(
     child: Repeate_contanier(
     colors: Color(0xFF1D1E33),
@@ -91,12 +102,7 @@ class _InputPageState extends State<InputPage> {
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
     Text('HEIGHT', style: ConstentTextLabel),
-    ],
-    ),
-    ),
-    ),
-    Expanded(
-    child: Row(
+    Row(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
     Text(
@@ -121,19 +127,139 @@ class _InputPageState extends State<InputPage> {
     });
     },
     )
-    );
-    }
-    }
-    6  lib/RepeateTextIcon.dart
-    @@ -1,4 +1,5 @@
-    import 'package:flutter/material.dart';
-    import 'constent_variable.dart';
-
-    class RepeateTextIcon extends StatelessWidget {
-    RepeateTextIcon({@required this.iconData, this.Label});
-    @@ -17,10 +18,7 @@ class RepeateTextIcon extends StatelessWidget {
-    SizedBox(height: 10),
+    ],
+    ),
+    ),
+    ),
+    Expanded(
+    child: Row(
+    children: [
+    Expanded(
+    child: Repeate_contanier(
+    cardWidge: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text(
+    'WEGHT',
     style: ConstentTextLabel,
+    ),
+    Text(slider_weight.toString(),
+    style: ConstentTextLabe2),
+
+
+
+    int sliderage = 10;
+
+    ],
+    ),
+    colors: Color(0xFF1D1E33),
+    ),
+    ),
+    Expanded(
+    child: Repeate_contanier(
+
+    Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    RoundIcon(
+    icondata: FontAwesomeIcons.minus,
+    onPress: () {
+    setState(() {
+    slider_weight--;
+    });
+    },
+    ),
+    SizedBox(width: 5),
+    RoundIcon(
+    icondata: FontAwesomeIcons.plus,
+    onPress: () {
+    setState(() {
+    slider_weight++;
+    });
+    },
+    ),
+    ],
     )
     ],
+    ),
+    colors: Color(0xFF1D1E33),
+    ),
+    ),
+    Expanded(
+    child: Repeate_contanier(
+    cardWidge: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text('AGE', style: ConstentTextLabel),
+    Text(sliderage.toString(), style: ConstentTextLabe2),
+    Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    RoundIcon(
+    icondata: FontAwesomeIcons.minus,
+    onPress: () {
+    setState(() {
+    sliderage--;
+    });
+    },
+    ),
+    SizedBox(width: 5),
+    RoundIcon(
+    icondata: FontAwesomeIcons.plus,
+    onPress: () {
+    setState(() {
+    sliderage++;
+    });
+    },
+    ),
+    ],
+    )
+    ],
+    ),
+
+
+    colors: Color(0xFF1D1E33),
+    ),
+    ),
+    ],
+    ),
+    ),
+          ],
+      ),
     );
+  }
+}
+
+Container(
+color: Colors.red,
+margin: EdgeInsets.only(top: 10),
+height: 80.0,
+)
+
+
+
+],
+),
+);
+}
+}
+
+class RoundIcon extends StatelessWidget {
+  RoundIcon({@required this.icondata, this.onPress});
+  final IconData icondata;
+  final Function onPress;
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(
+        icondata,
+        color: Colors.black,
+      ),
+      onPressed: onPress,
+      elevation: 6,
+      constraints: BoxConstraints.tightFor(height: 45.0, width: 45.0),
+      shape: CircleBorder(),
+      fillColor: Colors.white,
+    );
+  }
+}
