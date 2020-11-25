a990344ab6c6dcf23495eb9fc33dfd07240bb244
Khizar-PC <khizerdawood6@gmail.com>
2020-11-26 00:42:34 +0500

    USE OF ENUMERATION|| Lecture 61

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -1,96 +1,82 @@
-import 'package:flutter/material.dart';
-import 'Repeate_contanier.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'RepeateTextIcon.dart';
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
+enum Gender {
+  male,
+  female,
 }
 
+const activationcolor = Color(0xFF111328);
+const deactivationcolor = Color(0xFF1D1E33);
+
+@@ -14,12 +19,12 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Repeate_contanier(
-                    colors: Color(0xFF1D1E33),
-                    cardWidge: RepeateTextIcon(
-                      iconData: FontAwesomeIcons.male,
-                      Label: "MALE",
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Repeate_contanier(
-                    colors: Color(0xFF1D1E33),
-                    cardWidge: RepeateTextIcon(
-                      iconData: FontAwesomeIcons.female,
-                      Label: "FEMALE",
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Repeate_contanier(
-              colors: Color(0xFF1D1E33),
-            ),
-          ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Repeate_contanier(
-                    colors: Color(0xFF1D1E33),
-                  ),
-                ),
-                Expanded(
-                  child: Repeate_contanier(
-                    colors: Color(0xFF1D1E33),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+Color maleColor = deactivationcolor;
+Color femaleColor = deactivationcolor;
+void updateColor(int gender) {
+if (gender == 1) {
+void updateColor(Gender genderType) {
+if (genderType == Gender.male) {
+maleColor = activationcolor;
+femaleColor = deactivationcolor;
 }
-
-class RepeateTextIcon extends StatelessWidget {
-  RepeateTextIcon({@required this.iconData, this.Label});
-  final IconData iconData;
-  final String Label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          iconData,
-          //FontAwesomeIcons.male,
-          size: 50.0,
-        ),
-        SizedBox(height: 10),
-        Text(
-          Label,
-          style: TextStyle(
-            fontSize: 15,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-      ],
-    );
-  }
+if (gender == 2) {
+if (genderType == Gender.female) {
+maleColor = deactivationcolor;
+femaleColor = activationcolor;
+}
+@@ -40,7 +45,7 @@ class _InputPageState extends State<InputPage> {
+child: GestureDetector(
+onTap: () {
+setState(() {
+updateColor(1);
+updateColor(Gender.male);
+});
+},
+child: Repeate_contanier(
+@@ -56,7 +61,7 @@ class _InputPageState extends State<InputPage> {
+child: GestureDetector(
+onTap: () {
+setState(() {
+updateColor(2);
+updateColor(Gender.female);
+});
+},
+child: Repeate_contanier(
+colors: femaleColor,
+cardWidge: RepeateTextIcon(
+iconData: FontAwesomeIcons.female,
+Label: "FEMALE",
+),
+),
+),
+),
+],
+),
+),
+Expanded(
+child: Repeate_contanier(
+colors: Color(0xFF1D1E33),
+),
+),
+Expanded(
+child: Row(
+children: [
+Expanded(
+child: Repeate_contanier(
+colors: Color(0xFF1D1E33),
+),
+),
+Expanded(
+child: Repeate_contanier(
+colors: Color(0xFF1D1E33),
+),
+),
+],
+),
+),
+],
+),
+);
+}
 }
